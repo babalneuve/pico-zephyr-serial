52ecb586443eaa8998be70c4bc0fabab390b41aa
Vermast(EXT), Bastian <bastian.vermast.ext@barco.com>
2024-06-04 09:29:34 +0200

    Fonctionnel

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,17 @@
+empty_file.o: C:/Users/EXT_BASVER/zephyrproject/zephyr/misc/empty_file.c \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/boards/arm/rpi_pico/rpi_pico.dts \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/boards/arm/rpi_pico/rpi_pico-common.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/common/freq.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/arm/rpi_pico/rp2040.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/arm/armv6-m.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/regulator/rpi_pico.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/common/mem.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/dts/arm/rpi_pico/rpi_pico_common.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/boards/arm/rpi_pico/rpi_pico-pinctrl.dtsi \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/rpi-pico-rp2040-pinctrl.h \
+ C:/Users/EXT_BASVER/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ C:/Users/EXT_BASVER/zephyrproject/pico-zephyr-serial/app.overlay
